2f65a4b57e1b13fca767d53020f646a70f2ee17c
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-07-23 16:08:55 -0500

    Instalacion y configuracion de fos user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,8 @@ class AppKernel extends Kernel
             // And finally, the storage and SonataAdminBundle
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
