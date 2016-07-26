72ef9f93f982edff9a7ea3cb41eb4ff8f7b6ea1d
Cristian Angulo <cristianangulonova@gmail.com>
2016-07-26 16:37:51 +0000

    Friends module

--- src/AppBundle/Controller/FriendsController.php
+++ src/AppBundle/Controller/FriendsController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Blocks;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -60,5 +61,22 @@ class FriendsController extends Controller
         //'users' => $usersManager->getRepository()->findAll(),
         'user' => $user
       ));
+    }
+
+    /**
+     * @Route("/invitations-send/{guest}", name="friends_invitations_send")
+     */
+    public function invitationsSendAction(User $guest, Request $request)
+    {
+      $friendsManager = $this->get('friends.manager');
+
+      $friends = $friendsManager->create();
+
+      $friends->setHosts($this->getUser());
+      $friends->setGuests($guest);
+      $friends->setState(1);
+      $friendsManager->save($friends);
+
+      return $this->redirect($this->generateUrl('friends_search'));
     }
 }
